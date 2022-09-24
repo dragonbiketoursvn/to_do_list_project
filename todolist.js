e4bc31ec9f916ebf403fb8547a7191ec3ac405b8
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2022-09-24 18:16:43 +0700

    Added todolist class

--- todolist.js
+++ todolist.js
@@ -32,3 +32,69 @@ class Todo {
     return this.title;
   }
 }
+
+// This class represents a collection of Todo objects.
+// You can perform typical collection-oriented actions
+// on a TodoList object, including iteration and selection.
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+  add(todo) {
+    if (!(todo instanceof Todo)) {
+      throw new TypeError("can only add Todo objects");
+    }
+    this.todos.push(todo);
+  }
+  size() {
+    return this.todos.length;
+  }
+  first() {
+    return this.todos[0];
+  }
+  last() {
+    if (this.todos.length === 0) {
+      return undefined;
+    }
+    return this.todos.slice(-1);
+  }
+  _validateIndex(idx) {
+    if (this.todos[idx] === undefined) {
+      throw new ReferenceError(`Invalid index: ${idx}`);
+    } else {
+      return true;
+    }
+  }
+  itemAt(idx) {
+    this._validateIndex(idx);
+    return this.todos[idx];
+  }
+  markDoneAt(idx) {
+    this.itemAt(idx).markDone();
+  }
+  markUndoneAt(idx) {
+    this.itemAt(idx).markUndone();
+  }
+  isDone() {
+    return this.todos.every(todo => todo.isDone());
+  }
+  shift() {
+    return this.todos.pop();
+  }
+  pop() {
+    return this.todos.pop();
+  }
+  removeAt(idx) {
+    this._validateIndex(idx);
+    return this.todos.splice(idx, 1)[0];
+  }
+  toString() {
+    let result = '---- Today\'s Todos ----\n';
+    this.todos.forEach(todo => {
+      result += `${todo.toString()}\n`;
+    });
+    return result;
+  }
+}
